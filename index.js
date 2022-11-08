34a5bf1457dd3e4a969f4339ea34d2121869df27
elsamcgu <elsamcguinn18@gmail.com>
2022-11-08 20:30:54 +0000

    js added

---
+++ index.js
@@ -0,0 +1,11 @@
+const answer = prompt("will you be able to attend the party? yes/no ");
+
+if(answer='yes') {
+alert("you're welcome");
+}
+else if (answer =='no'){
+alert("hope to see you next time");
+}
+else {
+    alert('answer with a yes or no!')
+}
